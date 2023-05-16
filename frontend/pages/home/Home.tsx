00302771bef4af73180a007e9a17c81947af89f2
raphaelscandura <raphaelscandura@hotmail.com>
2023-05-16 15:47:45 -0300

    Creating homepage, tests now passing

---
+++ frontend/pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import Header from "../../components/Header/Header";
+
+const home = () => {
+    return (
+        <>
+            <Header/>
+            <h1>Página home inicial</h1>
+        </>
+    )
+}
+
+export default home
