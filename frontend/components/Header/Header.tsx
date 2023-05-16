b7aa8edc9d8425bca5777c37a6d02c5974f8a64a
raphaelscandura <raphaelscandura@hotmail.com>
2023-05-16 15:45:21 -0300

    creating first component, header

---
+++ frontend/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo" role="img" aria-label='Fatnezz logo'></div>
+        </header>
+    )
+}
+
+export default Header
