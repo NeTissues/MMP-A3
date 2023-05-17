331510db97cfa8e562976df88f68869005f02939
raphaelscandura <raphaelscandura@hotmail.com>
2023-05-17 14:34:27 -0300

    footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,8 @@
+import './Footer.css'
+const Footer = () => {
+    return (<footer className="footer">
+        Fatneez&copy; Todos os direitos reservados
+    </footer>)
+}
+
+export default Footer
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header";
 import React from "react";
+import Footer from "../../components/Footer/Footer";
 
 const home = () => {
     return (
         <>
             <Header/>
             <h1>Página home inicial</h1>
+            <Footer/>
         </>
     )
 }
